de808fb8b4c7f2dbc2f6696f2e82c619a0b8495e
amirmejri96 <amirmejri1996@gmail.com>
2025-03-19 14:14:26 +0100

    nettoyage frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "../src/pages/Home/HomePage";
-import LoginPage from "../src/pages/Login/LoginPage";
-import RegisterPage from "../src/pages/Register/RegisterPage";
-// import ProductPage from "./pages/ProductPage";
-// import CartPage from "./pages/CartPage";
-// import DashboardAdmin from "./pages/admin/DashboardAdmin";
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        {/* <Route path="/product/:id" element={<ProductPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/admin/dashboard" element={<DashboardAdmin />} /> */}
-      </Routes>
-    </Router>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
 
